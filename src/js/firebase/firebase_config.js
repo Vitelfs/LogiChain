6c0925a48345edbdec7c48b346534f7eb3c67f61
Breno Andrade <brebritto@gmail.com>
2024-08-20 21:01:13 -0300

    pagina de produtos 0.2

--- src/js/firebase/firebase_config.js
+++ src/js/firebase/firebase_config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getFirestore, collection, getDocs, addDoc, doc, setDoc, deleteDoc, Timestamp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signOut } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyC3U7YjxCDJCmjv_WeCmRr-MWk3m8PRi2Q",
@@ -12,10 +11,37 @@ const firebaseConfig = {
   appId: "1:940006643520:web:e932b7d8b591c40be18ca5"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
+export { signInWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signOut, Timestamp };
 
-export { signInWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signOut }
+export const products = collection(db, "produtos");
+
+export async function exportProducts() {
+  const querySnapshot = await getDocs(products);
+  return querySnapshot;
+}
+
+export function addDocuments(colecao, dados) {
+  return addDoc(collection(db, colecao), dados);
+}
+
+export function editDocuments(colecao, docId, novosDados) {
+  const docRef = doc(db, colecao, docId);
+  return setDoc(docRef, novosDados, { merge: true });
+}
+
+export function deleteDocuments(colecao, docId) {
+  const docRef = doc(db, colecao, docId);
+  return deleteDoc(docRef);
+}
+
+export async function exportDocs() {
+  const querySnapshotProdutos = await getDocs(products);
+  return {
+    produtos: querySnapshotProdutos
+  };
+}
